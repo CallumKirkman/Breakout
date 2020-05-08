b8ee467e2d2ce073693bfe1e3199229289d9c347
Callum laptop <callum@kirkman.co.uk>
2020-05-08 19:02:22 +0100

    Player update

--- app/src/main/java/com/example/breakout/Player.kt
+++ app/src/main/java/com/example/breakout/Player.kt
@@ -1,12 +1,35 @@
 package com.example.breakout
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Player : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
+    private var totalTime: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
+
+        mp = MediaPlayer.create(this, R.raw.temperature)
+        mp.isLooping = true
+        mp.setVolume(0.5F, 0.5F)
+        totalTime = mp.duration
+    }
+
+    fun playButtonClick(view: View) {
+
+        if (mp.isPlaying) {
+            //Stop
+            mp.pause()
+            //Make play button
+        } else {
+            //Start
+            mp.start()
+            //Make pause
+        }
     }
 }
