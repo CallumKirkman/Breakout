04245cbbc45622f25137ee4c8c40308401945620
jaykybert <51761831+jaykybert@users.noreply.github.com>
2020-05-09 18:31:19 +0100

    libs module with jdbc driver. work on db connection.

---
+++ account-res/src/main/java/com/example/account_res/OracleConnection.java
@@ -0,0 +1,32 @@
+package com.example.account_res;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class OracleConnection {
+
+    /*
+
+    public static void main(String[] args) throws Exception {
+        // Create jdcb connection object and load class.
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:oracle:thin:@192.168.0.22:1521:xe",
+                "system",
+                "password"
+        );
+
+        // Create Statement
+        Statement statement = con.createStatement();
+
+        // Execute
+        ResultSet rs = statement.executeQuery("SELECT * FROM simpletest");
+
+        while(rs.next()) {
+            System.out.println(rs.getString(1));
+        }
+    }
+     */
+}
