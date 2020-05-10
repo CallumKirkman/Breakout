533b2e65b5ad58edfae3840a3ee4a5fe5e3b6278
jaykybert <51761831+jaykybert@users.noreply.github.com>
2020-05-10 12:41:14 +0100

    db remote server update

--- account-res/src/main/java/com/example/account_res/OracleConnection.java
+++ account-res/src/main/java/com/example/account_res/OracleConnection.java
@@ -5,28 +5,24 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class OracleConnection {
 
-    /*
+public class OracleConnection {
 
     public static void main(String[] args) throws Exception {
         // Create jdcb connection object and load class.
         Class.forName("oracle.jdbc.driver.OracleDriver");
         Connection con = DriverManager.getConnection(
-                "jdbc:oracle:thin:@address:port:service",
-                "name",
-                "password"
+                "jdbc:oracle:thin:@computing.bournemouth.ac.uk:1521:decprd",
+                "s5107294_y1",
+                "breakout2020"
         );
 
-        // Create Statement
         Statement statement = con.createStatement();
 
-        // Execute
-        ResultSet rs = statement.executeQuery("SELECT * FROM simpletest");
+        ResultSet rs = statement.executeQuery("SELECT * FROM simple");
 
         while(rs.next()) {
             System.out.println(rs.getString(1));
         }
     }
-     */
 }
