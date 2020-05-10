3bf232cfd625dd94b57287696188a6f756cafaf7
Callum laptop <callum@kirkman.co.uk>
2020-05-10 20:28:49 +0100

    Kotlin version

--- app/src/main/java/com/example/breakout/PlayerActivity.kt
+++ app/src/main/java/com/example/breakout/PlayerActivity.kt
@@ -2,15 +2,65 @@ package com.example.breakout
 
 import android.annotation.SuppressLint
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.util.Log
 import android.view.View
 import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector.ConnectionListener
+import com.spotify.android.appremote.api.SpotifyAppRemote
 import kotlinx.android.synthetic.main.activity_player.*
 
 class PlayerActivity : AppCompatActivity() {
+
+    private var mSpotifyAppRemote: SpotifyAppRemote? = null
+
+    override fun onStart() {
+        super.onStart()
+        val connectionParams =
+            ConnectionParams.Builder(CLIENT_ID)
+                .setRedirectUri(REDIRECT_URI)
+                .showAuthView(true).build()
+        SpotifyAppRemote.connect(this, connectionParams,
+            object : ConnectionListener {
+                override fun onConnected(spotifyAppRemote: SpotifyAppRemote) {
+                    mSpotifyAppRemote = spotifyAppRemote
+                    Log.d("MainActivity", "Connected! Yay!")
+                    connected()
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Log.e("MainActivity", throwable.message, throwable)
+                }
+            })
+    }
+
+    private fun connected() {
+        // Play a playlist
+        mSpotifyAppRemote!!.playerApi.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
+
+        //Spotify song request - uses 62 base ID number at end of URI ^
+
+        //Either: request based if Intent from Player
+        //Or merge two files. Pretty sure I can
+        //Avoids calling and inheritance on each new call
+
+        //Most likely not needed in any other file?
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    companion object {
+        private const val CLIENT_ID = "daa95815630947bd980906b32437654d"
+        private const val REDIRECT_URI = "com.example.breakout:/callback"
+    }
+
+
 
     private lateinit var mp: MediaPlayer
     private var totalTime: Int = 0
