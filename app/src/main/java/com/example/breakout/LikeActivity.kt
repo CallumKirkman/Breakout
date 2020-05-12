e460131521e6edd4496a5345b298a8ef6490e32b
Callum laptop <callum@kirkman.co.uk>
2020-05-12 20:19:41 +0100

    Removal of RAW player & added additional tabs & navigation bar

---
+++ app/src/main/java/com/example/breakout/LikeActivity.kt
@@ -0,0 +1,35 @@
+package com.example.breakout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class LikeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_like)
+    }
+
+    //Menu bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.side_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_return) { //To menu
+            returnToMain()
+        }
+        return true
+    }
+
+    private fun returnToMain() {
+        val intent = Intent(this, PlayerActivity::class.java) //Starts Menu activity
+
+        startActivity(intent)
+    }
+}
--- app/src/main/java/com/example/breakout/PlayerActivity.kt
+++ app/src/main/java/com/example/breakout/PlayerActivity.kt
@@ -1,18 +1,23 @@
 package com.example.breakout
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector.ConnectionListener
 import com.spotify.android.appremote.api.SpotifyAppRemote
 import kotlinx.android.synthetic.main.activity_player.*
+
 
 class PlayerActivity : AppCompatActivity() {
 
@@ -47,25 +52,9 @@ class PlayerActivity : AppCompatActivity() {
 
     private fun connected() {
         // Play a playlist
-        spotifyAppRemote!!.playerApi.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
+        //spotifyAppRemote!!.playerApi.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
         // User will be given a pre-selected (or random) song for music preference, in case new
         // If they look up a song, or have previous preference data those songs will be used
-
-//        // Subscribe to PlayerState
-//        spotifyAppRemote!!.playerApi.playerState
-//            .setResultCallback {
-//                Log.d("PlayerActivity", "Connected")
-//            }
-//            .setErrorCallback {
-//                Log.e("PlayerActivity", "Fail")
-//            }
-
-
-//        // Subscribe to PlayerState
-//        spotifyAppRemote!!.playerApi.subscribeToPlayerState().setEventCallback {
-//            val track: Track = it.track
-//            Log.d("MainActivity", track.name + " by " + track.artist.name)
-//        }
     }
 
     override fun onStop() {
@@ -77,82 +66,32 @@ class PlayerActivity : AppCompatActivity() {
     }
 
 
-    private lateinit var mp: MediaPlayer
-    private var totalTime: Int = 0
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when(item.itemId){
+            R.id.navHome -> {
+                println("Home clicked")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navFavourites -> {
+                println("Favourites clicked")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navSettings -> {
+                println("Settings clicked")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+
     private var playPause = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
 
-        mp = MediaPlayer.create(this, R.raw.temperature)
-        mp.isLooping = true
-        mp.setVolume(0.5F, 0.5F)
-        totalTime = mp.duration
-
-
-        // Position bar
-        positionBar.max = totalTime
-        positionBar.setOnSeekBarChangeListener(
-            object  : SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(
-                    seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                    if (fromUser) {
-                        mp.seekTo(progress)
-                    }
-                }
-                override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
-                }
-                override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
-                }
-
-            }
-        )
-
-        @SuppressLint("HandlerLeak")
-        var handler = object : Handler() {
-            override fun handleMessage(msg: Message) {
-                super.handleMessage(msg)
-
-                var currentPosition = msg.what
-
-                // Update position bar
-                positionBar.progress = currentPosition
-
-                // Update Labels
-                var elapsedTime = createTimeLabel(currentPosition)
-                elapsedTimeLabel.text = elapsedTime
-
-                var remainingTime = createTimeLabel(totalTime - currentPosition)
-                remainingTimeLabel.text = "-$remainingTime"
-            }
-
-            private fun createTimeLabel(time: Int): String {
-                var timeLabel = ""
-                var min = time / 1000 / 60
-                var sec = time / 1000 % 60
-
-                timeLabel = "$min"
-                if (sec < 10) timeLabel += "0"
-                timeLabel += sec
-
-                return timeLabel
-            }
-        }
-
-        // Thread
-        Thread(Runnable {
-            while (mp != null) {
-                try {
-                    var message = Message()
-                    message.what = mp.currentPosition
-                    handler.sendMessage(message)
-                    Thread.sleep(1000)
-                } catch (e:InterruptedException) {}
-            }
-        }).start()
+        bottomNavigation.setOnNavigationItemReselectedListener { onNavigationItemSelectedListener }
     }
 
 
@@ -171,5 +110,19 @@ class PlayerActivity : AppCompatActivity() {
             playButton.setBackgroundResource(R.drawable.stop)
             spotifyAppRemote!!.playerApi.resume()
         }
+    }
+
+    fun likeButtonClick(view: View) {
+        // Add to favourite songs
+        val intent = Intent(this, LikeActivity::class.java) //Starts Like activity
+
+        startActivity(intent)
+    }
+
+    fun dislikeButtonClick(view: View) {
+        // Remove from play list?
+        val intent = Intent(this, DislikeActivity::class.java) //Starts Dislike activity
+
+        startActivity(intent)
     }
 }
