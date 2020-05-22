ca46465d745ee9ae559c76c358a4625ce59475e1
jaykybert <51761831+jaykybert@users.noreply.github.com>
2020-05-22 17:29:46 +0100

    More instrumentation tests.

--- app/src/androidTest/java/com/example/breakout/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/breakout/LoginActivityTest.java
@@ -1,22 +1,32 @@
 package com.example.breakout;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 public class LoginActivityTest {
 
     @Rule
-    public ActivityTestRule<LoginActivity> loginTestRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+    public ActivityTestRule<LoginActivity> loginTestRule = new ActivityTestRule<>(LoginActivity.class);
 
     private LoginActivity loginAct = null;
 
@@ -30,37 +40,58 @@ public class LoginActivityTest {
      * Ensure all components exist and are accessible in the Activity.
      */
     @Test
-    public void testComponentsAvailable() {
-        TextView title = loginAct.findViewById(R.id.Breakout);
-        assertNotNull(title);
-
-        EditText email = loginAct.findViewById(R.id.enterEmail);
-        assertNotNull(email);
-
-        EditText password = loginAct.findViewById(R.id.enterPassword);
-        assertNotNull(password);
-
-        TextView link = loginAct.findViewById(R.id.createAccountLink);
-        assertNotNull(link);
-
-        Button login = loginAct.findViewById(R.id.loginButton);
-        assertNotNull(login);
+    public void testAllComponentsDisplayed() {
+        onView(withId(R.id.Breakout)).check(matches(isDisplayed()));
+        onView(withId(R.id.enterEmail)).check(matches(isDisplayed()));
+        onView(withId(R.id.enterPassword)).check(matches(isDisplayed()));
+        onView(withId(R.id.createAccountLink)).check(matches(isDisplayed()));
     }
 
 
-    @After
-    public void tearDown() throws Exception {
-        loginAct = null;
+    /**
+     * Run this one.
+     */
+    @Test
+    public void testComponentsAcceptInput() {
+        onView(withId(R.id.enterEmail)).perform(typeText("example@email.com"));
+        onView(withId(R.id.enterPassword)).perform(typeText("ExamplePassword123!"));
+        onView(withId(R.id.loginButton)).perform(click());
     }
 
+
+    @Test
+    public void testEmailAcceptsInput() {
+        onView(withId(R.id.enterEmail)).perform(typeText("example@email.com"));
+    }
+
+    @Test
+    public void testPasswordAcceptsInput() {
+        onView(withId(R.id.enterPassword)).perform(typeText("ExamplePassword123!"));
+    }
 
 
 
     @Test
-    public void onCreate() {
+    public void testButtonRejectsEmptyData() {
+
+
+
+
+
     }
 
+    /**
+     * Supply a valid, created account. See if an Intent is launched.
+     */
     @Test
-    public void onClickLogin() {
+    public void testButtonAcceptsValidData() {
+        onView(withId(R.id.enterPassword)).perform(typeText("ExamplePassword123!"));
+        onView(withId(R.id.enterEmail)).perform(typeText("example@email.com"));
+        onView(withId(R.id.loginButton)).perform(click());
+
+        intended(hasComponent(PlayerActivity.class.getName()));
     }
+
+
+
 }
