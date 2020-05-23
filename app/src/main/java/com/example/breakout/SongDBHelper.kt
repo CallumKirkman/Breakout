424a30d766a838a7fe3dcf36424d07a2aa5ed809
JoeFleetwood <joe.fleetwood@live.co.uk>
2020-05-23 11:21:32 +0100

    working on like db

---
+++ app/src/main/java/com/example/breakout/SongDBHelper.kt
@@ -0,0 +1,20 @@
+package com.example.breakout
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+
+public class SongDBHelper(context: Context) {
+//
+//    private val
+//    private val
+//
+//
+//     public  fun writeLikedSongToDB(String song_Name, int song_Liked, String artist_Name, String song_URI, String image_Uri) {
+//
+//
+//
+//    }
+
+
+}
